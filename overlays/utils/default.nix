a8da70982abfc8e96ca0083931719a71d8d7125a
lucasew <lucas59356@gmail.com>
2020-08-25 21:44:14 -0300

    add comando que transforma imagem em uma representação de um pixel
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- overlays/utils/default.nix
+++ overlays/utils/default.nix
@@ -2,5 +2,6 @@ self: super: {
     utils = {
         importAllIn = import ./importAllIn.nix;
         lsName = import ./lsName.nix;
+        image2color = import ./image2color.nix;
     };
 }
---
+++ overlays/utils/image2color.nix
@@ -0,0 +1,11 @@
+image:
+let
+    pkgs = import <dotfiles/pkgs.nix>;
+    convert = "${pkgs.imagemagick}/bin/convert";
+    runCommand = pkgs.runCommand;
+    pixelFile = runCommand "img2pixel" {} ''
+        ${convert} ${image} -resize 1x1\! txt:- | sed 's/\ /\n/g' | grep '#[0-9A-F]' > $out
+    '';
+    pixelString = builtins.readFile "${pixelFile}";
+    stripped = builtins.replaceStrings ["\n"] [""] pixelString;
+in stripped
