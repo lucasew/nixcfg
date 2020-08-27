f8d89eb16f2348aed85197a7f95a31526aa8dd7a
lucasew <lucas59356@gmail.com>
2020-08-27 11:51:26 -0300

    Minecraft funcionando com som e tudo nessa caraia
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- overlays/minecraft.nix
+++ overlays/minecraft.nix
@@ -6,6 +6,51 @@ let
         sha1 = "61cb768106e6e449158ebb2608ad1327402d9fec";
         url = "https://teamshiginima.com/update/";
     };
+    envLibPath = with pkgs; stdenv.lib.makeLibraryPath [
+        curl
+        libpulseaudio
+        systemd
+        alsaLib # needed for narrator
+        flite # needed for narrator
+        xorg.libXxf86vm # needed only for versions <1.13
+        xorg.libX11
+        libGLU libGL xorg.libXpm xorg.libXext alsaLib
+    ];
+    libPath = stdenv.lib.makeLibraryPath (with pkgs;[
+        alsaLib
+        atk
+        cairo
+        cups
+        dbus
+        expat
+        fontconfig
+        freetype
+        gdk-pixbuf
+        glib
+        gnome2.GConf
+        gnome2.pango
+        gtk3-x11
+        gtk2-x11
+        nspr
+        nss
+        stdenv.cc.cc
+        zlib
+        libuuid
+  ] ++
+  (with xorg; [
+    libX11
+    libxcb
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libXScrnSaver
+  ]));
     drv = pkgs.stdenv.mkDerivation rec {
         name = "minecraft";
         src = launcherZip;
@@ -25,7 +70,10 @@ let
             do
                 cat "$file" > $out/share/java/minecraft.jar
             done
-            makeWrapper ${jre8}/bin/java $out/bin/minecraft --add-flags "-jar $out/share/java/minecraft.jar"
+            # makeWrapper ${pkgs.latest.oraclejre}/bin/java $out/bin/minecraft \
+            makeWrapper ${pkgs.jre8}/bin/java $out/bin/minecraft \
+                --add-flags "-jar $out/share/java/minecraft.jar" \
+                      --prefix LD_LIBRARY_PATH : ${envLibPath}
         '';
         meta = {
             homepage = "https://teamshiginima.com/update/";
