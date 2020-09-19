64dd410ed04cd067be2bbde390649e47c1ca7f58
lucasew <lucas59356@gmail.com>
2020-09-19 11:25:02 -0300

    generate .nix-channels using home-manager
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ home/components/nix-channels.nix
@@ -0,0 +1,8 @@
+{...}:
+{
+  home.file.".nix-channels".text = ''
+https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+https://nixos.org/channels/nixos-unstable nixos-unstable
+https://github.com/nix-community/NUR/archive/master.tar.gz nur
+  '';
+}
