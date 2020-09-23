6217f9286040a935f1eeaace13d89b0fb0c378fd
lucasew <lucas59356@gmail.com>
2020-09-22 21:35:44 -0300

    add kdenlive e gimp, fix conflito em pkgs.utils que não deixava compilar o kdenlive
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ home/components/edicao_midia.nix
@@ -0,0 +1,7 @@
+{pkgs,...}:
+{
+  home.packages = [
+    pkgs.kdeApplications.kdenlive
+    pkgs.gimp
+  ];
+}
--- overlays/utils/default.nix
+++
@@ -1,7 +0,0 @@
-self: super: {
-    utils = {
-        importAllIn = import ./importAllIn.nix;
-        lsName = import ./lsName.nix;
-        image2color = import ./image2color.nix;
-    };
-}
