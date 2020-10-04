d622fbe10cd28091415c1c073d5778a532ada49e
lucasew <lucas59356@gmail.com>
2020-10-04 12:43:28 -0300

    add a few gnome extensions
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- machine/acer/components/gui/gnome.nix
+++ machine/acer/components/gui/gnome.nix
@@ -4,6 +4,11 @@ in
 {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.night-theme-switcher
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.gsconnect
+  ];
   # xdg.portal= {
   #   enable = true;
   #   gtkUsePortal = true;
