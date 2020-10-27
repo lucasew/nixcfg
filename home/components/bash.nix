38f137e4e9ce1ba5994adc300901912632abe0be
lucasew <lucas59356@gmail.com>
2020-10-27 11:14:08 -0300

    add pasta bin global do yarn na PATH
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- home/components/bash.nix
+++ home/components/bash.nix
@@ -8,6 +8,7 @@ in
     initExtra = ''
       export EDITOR="nvim"
       export PS1="\u@\h \w \$?\\$ \[$(tput sgr0)\]"
+      export PATH="$PATH:~/.yarn/bin/"
       source ~/.dotfilerc
       alias la="ls -a"
       alias ncdu="rclone ncdu . 2> /dev/null"
