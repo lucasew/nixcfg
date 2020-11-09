1ceb6ba2254c97e5eaeffae830c5ca4c246a1168
lucasew <lucas59356@gmail.com>
2020-11-09 12:22:56 -0300

    dont send env file to nix-store
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- modules/nixgram/service.nix
+++ modules/nixgram/service.nix
@@ -2,7 +2,7 @@
 let
   bin = pkgs.writeShellScript "nixgram-service" ''
     echo "Iniciando..."
-    ${pkgs.dotenv}/bin/dotenv @${../../secrets/nixgram.env} -- ${pkgs.nixgram}/bin/nixgram
+    ${pkgs.dotenv}/bin/dotenv @${builtins.toString ../../secrets/nixgram.env} -- ${pkgs.nixgram}/bin/nixgram
   '';
   systemdUserService = import <dotfiles/lib/systemdUserService.nix>;
 in systemdUserService {
