4845c7208a41de24f5baf45457ef4b9bec955aee
lucasew <lucas59356@gmail.com>
2020-11-30 08:37:35 -0300

    build.nix: converting everything to a list
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- build.nix
+++ build.nix
@@ -1,7 +1,9 @@
 let
   globalConfig = import ./globalConfig.nix;
   nixpkgs = import globalConfig.repos.nixpkgs {};
-in {
+in 
+with builtins;
+(attrValues {
   comby = import ./modules/comby/package.nix;
   rofi = import ./modules/custom_rofi/package.nix;
   dotenv = import ./modules/dotenv/package.nix;
@@ -11,11 +13,11 @@ in {
   pinball = import ./modules/pinball/package.nix;
   stremio = import ./modules/stremio/package.nix;
   yt = import ./modules/youtube/package.nix;
-  zls = (import ./modules/zig/overlay.nix).zls;
+  zls = (import ./modules/zig/overlay.nix nixpkgs nixpkgs).zls;
   among_us = import ./nodes/acer-nix/modules/among_us/package.nix;
   ets2 = import ./nodes/acer-nix/modules/ets2/package.nix;
   usb_tixati = import ./nodes/acer-nix/modules/usb_tixati/package.nix;
 
-}
-// (import ./modules/vscode/extensions.nix {pkgs = nixpkgs;})
-// (import ./nodes/acer-nix/modules/webviews/overlay.nix).nodePackages
+})
+++ (import ./modules/vscode/extensions.nix {pkgs = nixpkgs;})
+++ (attrValues (import ./nodes/acer-nix/modules/webviews/overlay_data/composition.nix {pkgs = nixpkgs;}))
