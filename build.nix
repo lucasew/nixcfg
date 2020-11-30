26447c7d52a564d117a15d83d03ba1b11a813008
lucasew <lucas59356@gmail.com>
2020-11-30 08:27:43 -0300

    passing packages instead the whole system in build.nix
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- build.nix
+++ build.nix
@@ -1,7 +1,21 @@
 let
   globalConfig = import ./globalConfig.nix;
   nixpkgs = import globalConfig.repos.nixpkgs {};
-  buildDefinitions = {
-    acer-nix = nixpkgs.nixos ./nodes/acer-nix/default.nix;
-  };
-in buildDefinitions.acer-nix
+in {
+  comby = import ./modules/comby/package.nix;
+  rofi = import ./modules/custom_rofi/package.nix;
+  dotenv = import ./modules/dotenv/package.nix;
+  mspaint = import ./modules/mspaint/package.nix;
+  neovim = import ./modules/neovim/package.nix;
+  a22120 = import ./modules/node_clis/package_22120.nix;
+  pinball = import ./modules/pinball/package.nix;
+  stremio = import ./modules/stremio/package.nix;
+  yt = import ./modules/youtube/package.nix;
+  zls = (import ./modules/zig/overlay.nix).zls;
+  among_us = import ./nodes/acer-nix/modules/among_us/package.nix;
+  ets2 = import ./nodes/acer-nix/modules/ets2/package.nix;
+  usb_tixati = import ./nodes/acer-nix/modules/usb_tixati/package.nix;
+
+}
+// (import ./modules/vscode/extensions.nix {pkgs = nixpkgs;})
+// (import ./nodes/acer-nix/modules/webviews/overlay.nix).nodePackages
