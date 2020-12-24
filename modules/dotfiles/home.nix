1e5d0bca30cd28d6dd41405aaebfc07d4f815ff7
lucasew <lucas59356@gmail.com>
2020-12-24 15:24:52 -0300

    flakes
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- modules/dotfiles/home.nix
+++ modules/dotfiles/home.nix
@@ -1,11 +1,10 @@
 { pkgs, ...}:
 let
   globalConfig = import <dotfiles/globalConfig.nix>;
-  generator = import <dotfiles/lib/generateDotfilerc.nix>;
 in
 {
   home.file.".dotfilerc".text = ''
     #!/usr/bin/env bash
-    ${generator globalConfig}
+    alias nixos-rebuild="sudo -E nixos-rebuild --flake '${builtins.toString ./.}#acer-nix'"
   '';
 }
