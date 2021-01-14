f56b71a0b8e3c2c32df92b38fa4528a247863a8e
lucasew <lucas59356@gmail.com>
2021-01-14 16:05:21 -0300

    globalConfig: this project flake as a value
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- globalConfig.nix
+++ globalConfig.nix
@@ -7,4 +7,5 @@ rec {
       url = "http://wallpaperswide.com/download/aurora_sky-wallpaper-1366x768.jpg";
       sha256 = "1gk4bw5mj6qgk054w4g0g1zjcnss843afq5h5k0qpsq9sh28g41a";
   };
+  flake = import ./lib/flake {};
 }
