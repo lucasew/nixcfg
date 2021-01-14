451b4cd6b83be85c49795deb2e24aa3f4bcf56ce
lucasew <lucas59356@gmail.com>
2021-01-14 18:23:37 -0300

    flake: simplify devShell
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- flake.nix
+++ flake.nix
@@ -29,14 +29,13 @@
   let
     userSettings = import ./globalConfig.nix;
     system = "x86_64-linux";
-    environmentShell = ''
-      alias nixos-rebuild="sudo -E nixos-rebuild --flake '${userSettings.rootPath}#acer-nix'"
+    environmentShell = with userSettings; ''
+      alias nixos-rebuild="sudo -E nixos-rebuild --flake '${rootPath}#acer-nix'"
       export NIXPKGS_ALLOW_UNFREE=1
-      export NIX_PATH="nixos-config=${(builtins.toString userSettings.rootPath) + "/nodes/acer-nix"}:nixpkgs=${nixpkgs}:dotfiles=${builtins.toString userSettings.rootPath}:nixpkgsLatest=${nixpkgsLatest} home-manager=${home-manager}:nur=${nur}"
+      export NIX_PATH="nixos-config=${(builtins.toString rootPath) + "/nodes/acer-nix"}:nixpkgs=${nixpkgs}:dotfiles=${builtins.toString rootPath}:nixpkgsLatest=${nixpkgsLatest} home-manager=${home-manager}:nur=${nur}"
 
   '';
     overlays = [
-      # (import ./modules/node_clis/overlay.nix)
       (import ./modules/zig/overlay.nix)
       (import ./overlay.nix)
     ];
