4437f33d9ade96353ae5e221113d3d282d014cf6
lucasew <lucas59356@gmail.com>
2021-03-17 11:51:07 -0300

    add chrome appmode
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ packages/appmode.nix
@@ -0,0 +1,21 @@
+{pkgs ? import <nixpkgs> {}}:
+let
+  zenity = "${pkgs.gnome3.zenity}/bin/zenity";
+  chrome = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  script = with pkgs; ''
+    if [ -z "$@" ]
+    then
+      URL=$(${zenity} --entry --text="Link a ser aberto")
+    else
+      URL="$@"
+    fi
+    if [ -z "$URL" ]
+    then
+      ${zenity} --error --text="Nenhuma URL especificada"
+      exit 1
+    fi
+    [[ "$URL" == '^http(s){0,1}:\/\/' ]] || URL="https://$URL"
+    echo $URL
+    ${chrome} --app="$URL"
+  '';
+in pkgs.writeShellScriptBin "webapp" script
