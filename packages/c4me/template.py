01725a30271091a35768a912927dce26f14d0a0d
lucasew <lucas59356@gmail.com>
2021-03-20 13:53:23 -0300

    b64d, b64e: fix
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- packages/c4me/template.py
+++ packages/c4me/template.py
@@ -1,16 +1,27 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p python3 -i python
 
-from base64 import b64encode, b64decode
 from math import *
+
+def b64d(data):
+    from base64 import b64decode
+    decoded = b64decode(data)
+    return decoded.decode('utf-8')
+
+def b64e(data):
+    from base64 import b64encode
+    raw = bytes(data, 'utf-8')
+    encoded = b64encode(raw)
+    return encoded.decode('utf-8')
+
 
 def debug_mode():
     return getenv("DEBUG") != None
 
-def eval_print(stmt):
+def eval_print(stmt, globals=globals(), locals=locals()):
     if debug_mode():
         print("Run statement: ", stmt)
-    print(eval(stmt))
+    print(eval(stmt, globals, locals))
 
 def getenv(key):
     from os import environ
@@ -24,6 +35,7 @@ def is_match(regex, text):
 def get_args():
     from sys import argv
     return argv[1:]
+
 def eval_passed_stmt():
     args = get_args()
     if len(args) == 0:
@@ -37,7 +49,7 @@ def eval_passed_stmt():
         eval_print(" ".join(args))
     elif not is_match(r"^.*\(", args[0]):
         [fn, *args] = args
-        eval_print("%s( %s )" %(fn, ", ".join(args)))
+        eval_print("%s(*args)" %(fn), globals(), locals())
     elif len(args) == 1:
         eval_print(args[0])
 def repl():
