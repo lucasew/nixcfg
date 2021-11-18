9f85afcdb2400c347baefe8d33114c9be28c1a68
lucasew <lucas59356@gmail.com>
2021-11-18 16:00:30 -0300

    chore: using a default.nix to make it compatible to bootstrap in non flake environments
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- build.nix
+++ build.nix
@@ -1,30 +1,23 @@
-let
-  inherit (builtins) getFlake;
-  flake = getFlake "${toString ./.}";
-  inherit (flake.outputs) pkgs;
-in builtins.attrValues {
-  inherit (pkgs.x86_64-linux)
+with import ./default.nix;
+builtins.attrValues {
+  inherit (pkgs)
     stremio
     minecraft
     discord
   ;
-  # inherit (pkgs.aarch64-linux.custom)
-  #   neovim
-  #   emacs
-  # ;
-  inherit (pkgs.x86_64-linux.python3Packages)
+  inherit (pkgs.python3Packages)
     scikitlearn
   ;
-  inherit (pkgs.x86_64-linux.wineApps)
+  inherit (pkgs.wineApps)
     wine7zip
     pinball
   ;
-  polybar = pkgs.x86_64-linux.callPackage ./modules/polybar/customPolybar.nix {};
-  inherit (flake.outputs.nixosConfigurations.x86_64-linux)
+  polybar = pkgs.callPackage ./modules/polybar/customPolybar.nix {};
+  inherit (nixosConfigurations)
     acer-nix
     vps
   ;
-  inherit (flake.outputs.homeConfigurations.x86_64-linux)
+  inherit (homeConfigurations)
     main
   ;
 }
