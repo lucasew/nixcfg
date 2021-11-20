7497b92e345805ca8f0e1f85221801da6aff30e6
lucasew <lucas59356@gmail.com>
2021-11-19 21:36:59 -0300

    add xiaomi stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/xiaomi/default.nix
+++ nodes/xiaomi/default.nix
@@ -1,2 +1,25 @@
 {pkgs, config, options, ...}:
-{}
+{
+  system.stateVersion = "21.05";
+  time.timeZone = "America/Sao_Paulo";
+  home-manager = {
+    useGlobalPkgs = true;
+    config = {...}: {
+      home.stateVersion = "21.05";
+    };
+  };
+  nix = {
+    package = pkgs.nix;
+    extraConfig = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+  environment.packages = with pkgs; [
+    nix-option
+    custom.neovim
+    custom.emacs
+    git
+    htop
+    pkg
+  ];
+}
