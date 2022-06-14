470023f64900bbd5ec076f89abf5fba686f62505
lucasew <lucas59356@gmail.com>
2022-06-13 21:06:54 -0300

    vps: revivendo o veio
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/vps/default.nix
+++ nodes/vps/default.nix
@@ -13,20 +13,32 @@ in {
     ./modules
   ];
 
+  fileSystems = {
+    "/persist" = {
+      neededForBoot = true;
+      device = "/dev/sdb1";
+      fsType = "ext4";
+    };
+  };
+
   vps = {
     domain = "ztvps.biglucas.tk";
     # alibot.enable = true;
-    pgbackup.enable = true;
+    # pgbackup.enable = true;
   };
   
   environment.persistence."/persist" = {
+    hideMounts = true;
     files = [
       "/etc/machine-id"
     ];
     directories = [
-      "${toString rootPath}/secrets"
+      "${toString rootPath}"
       "/backups"
       "/srv/php-utils"
+    ];
+    users.${username}.directories = [
+      "WORKSPACE"
     ];
   };
 
@@ -63,21 +75,21 @@ in {
   };
   virtualisation.docker.enable = true;
   services = {
-    dnsmasq = {
-      enable = true;
-      resolveLocalQueries = true;
-    };
+    # dnsmasq = {
+    #   enable = true;
+    #   resolveLocalQueries = true;
+    # };
     irqbalance.enable = true;
-    php-utils.enable = true;
+    # php-utils.enable = true;
     # vaultwarden.enable = true;
-    postgresql = {
-      enable = true;
-      enableTCPIP = true;
-      authentication = mkOverride 10 ''
-        local all all trust
-        host all all 192.168.69.0/24 trust
-      '';
-    };
+    # postgresql = {
+    #   enable = true;
+    #   enableTCPIP = true;
+    #   authentication = mkOverride 10 ''
+    #     local all all trust
+    #     host all all 192.168.69.0/24 trust
+    #   '';
+    # };
     # randomtube = { # TODO: Bump git commit
     #   enable = false;
     #   extraParameters = "-ms 120";
@@ -88,9 +100,9 @@ in {
   cachix.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_5_4;
-  services.nginx = {
-    enable = true;
-    enableReload = true;
-  };
+  # services.nginx = {
+  #   enable = true;
+  #   enableReload = true;
+  # };
   system.stateVersion = "20.03";
 }
