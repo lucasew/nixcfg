7ea49854fb46d956ccfe8f65d763f5235df5b9e0
lucasew <lucas59356@gmail.com>
2022-06-22 18:20:31 -0300

    firefox: whatever
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- packages/custom/firefox.nix
+++ packages/custom/firefox.nix
@@ -3,31 +3,33 @@
 , stable
 , firefox-esr-91-unwrapped
 , fetchFirefoxAddon
+, firefox-bin
 , }:
-wrapFirefox firefox-esr-91-unwrapped {
-  desktopName = "Firefox (wrapped)";
-  applicationName = "firefox";
-  extraExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
-    inherit (e) name;
-    url = builtins.elemAt e.src.urls 0;
-    sha256 = e.src.outputHash;
-  }) [
-    darkreader
-    facebook-container
-    grammarly
-    i-dont-care-about-cookies
-    sponsorblock
-    tampermonkey
-    ublock-origin
-    xbrowsersync
-  ];
-  extraPolicies = {
-    DisableFirefoxStudies = true;
-    DisablePocket = true;
-    DisableTelemetry = true;
-    UserMessaging = {
-      ExtensionRecommendations = false;
-      SkipOnboarding = true;
-    };
-  };
-}
+firefox-bin
+# wrapFirefox firefox-esr-91-unwrapped {
+#   desktopName = "Firefox (wrapped)";
+#   applicationName = "firefox";
+#   extraExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
+#     inherit (e) name;
+#     url = builtins.elemAt e.src.urls 0;
+#     sha256 = e.src.outputHash;
+#   }) [
+#     darkreader
+#     facebook-container
+#     grammarly
+#     i-dont-care-about-cookies
+#     sponsorblock
+#     tampermonkey
+#     ublock-origin
+#     xbrowsersync
+#   ];
+#   extraPolicies = {
+#     DisableFirefoxStudies = true;
+#     DisablePocket = true;
+#     DisableTelemetry = true;
+#     UserMessaging = {
+#       ExtensionRecommendations = false;
+#       SkipOnboarding = true;
+#     };
+#   };
+# }
