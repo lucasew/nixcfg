c184a9b15dc96d15d1951d28505d0ae9aae0c092
lucasew <lucas59356@gmail.com>
2022-06-22 18:21:41 -0300

    firefox: oh no it seems that nix-env trolled me again
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- packages/custom/firefox.nix
+++ packages/custom/firefox.nix
@@ -5,31 +5,31 @@
 , fetchFirefoxAddon
 , firefox-bin
 , }:
-firefox-bin
-# wrapFirefox firefox-esr-91-unwrapped {
-#   desktopName = "Firefox (wrapped)";
-#   applicationName = "firefox";
-#   extraExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
-#     inherit (e) name;
-#     url = builtins.elemAt e.src.urls 0;
-#     sha256 = e.src.outputHash;
-#   }) [
-#     darkreader
-#     facebook-container
-#     grammarly
-#     i-dont-care-about-cookies
-#     sponsorblock
-#     tampermonkey
-#     ublock-origin
-#     xbrowsersync
-#   ];
-#   extraPolicies = {
-#     DisableFirefoxStudies = true;
-#     DisablePocket = true;
-#     DisableTelemetry = true;
-#     UserMessaging = {
-#       ExtensionRecommendations = false;
-#       SkipOnboarding = true;
-#     };
-#   };
-# }
+wrapFirefox firefox-esr-91-unwrapped {
+  desktopName = "Firefox (wrapped)";
+  applicationName = "firefox";
+  nixExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
+    inherit (e) name;
+    url = builtins.elemAt e.src.urls 0;
+    sha256 = e.src.outputHash;
+  }) [
+    darkreader
+    facebook-container
+    grammarly
+    i-dont-care-about-cookies
+    sponsorblock
+    tampermonkey
+    ublock-origin
+    xbrowsersync
+  ];
+  extraPolicies = {
+    DisableFirefoxStudies = true;
+    DisablePocket = true;
+    DisableTelemetry = true;
+    UserMessaging = {
+      ExtensionRecommendations = false;
+      SkipOnboarding = true;
+    };
+  };
+}
+
