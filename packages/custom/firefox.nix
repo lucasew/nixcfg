b31d669fef31ee38aa34bb335871ec546e09431a
lucasew <lucas59356@gmail.com>
2022-06-22 17:56:50 -0300

    firefox: whatever, just use firefox's system to manage extensions
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- packages/custom/firefox.nix
+++ packages/custom/firefox.nix
@@ -7,20 +7,20 @@
 wrapFirefox firefox-esr-91-unwrapped {
   desktopName = "Firefox (wrapped)";
   applicationName = "firefox";
-  nixExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
-    inherit (e) name;
-    url = builtins.elemAt e.src.urls 0;
-    sha256 = e.src.outputHash;
-  }) [
-    darkreader
-    facebook-container
-    grammarly
-    i-dont-care-about-cookies
-    sponsorblock
-    tampermonkey
-    ublock-origin
-    xbrowsersync
-  ];
+  # nixExtensions = with nur.repos.rycee.firefox-addons; map (e: fetchFirefoxAddon {
+  #   inherit (e) name;
+  #   url = builtins.elemAt e.src.urls 0;
+  #   sha256 = e.src.outputHash;
+  # }) [
+  #   darkreader
+  #   facebook-container
+  #   grammarly
+  #   i-dont-care-about-cookies
+  #   sponsorblock
+  #   tampermonkey
+  #   ublock-origin
+  #   xbrowsersync
+  # ];
   extraPolicies = {
     DisableFirefoxStudies = true;
     DisablePocket = true;
