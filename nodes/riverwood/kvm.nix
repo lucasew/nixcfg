6196bda93143343159359151cbf5b79d0ebcb743
lucasew <lucas59356@gmail.com>
2022-07-25 17:52:44 -0300

    riverwood/kvm: vgpu uuid
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/riverwood/kvm.nix
+++ nodes/riverwood/kvm.nix
@@ -7,7 +7,16 @@ in
     extraGroups = [ "kvm" "libvirt" ];
   };
   virtualisation = {
-    kvmgt.enable = true;
+    kvmgt = {
+      enable = true;
+      vgpus = {
+        "i91-GVTg_V5_8" = {
+          uuid = [
+            "130e9604-32a2-4824-9d47-34b3f6e0c857"
+          ];
+        };
+      };
+    };
     libvirtd.enable = true;
   };
   environment.systemPackages = with pkgs; [
