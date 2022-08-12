26401ca6418dc156049a753450143a5fce758f7e
lucasew <lucas59356@gmail.com>
2022-08-12 16:27:29 -0300

    home/main: move themes.nix to its own folder to split stuff in different modules
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- homes/main/theme/telegram.nix
+++ homes/main/theme/telegram.nix
@@ -1,68 +1,8 @@
-{ pkgs, config, self, ... }:
+{ pkgs, ... }:
 let
-  inherit (pkgs.custom.colors-lib-contrib) gtkThemeFromScheme shellThemeFromScheme;
-  inherit (builtins) concatStringsSep;
   inherit (pkgs.custom) colors;
   inherit (colors.colors) base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F;
-in
-{
-  home.packages = with pkgs; [
-    lxappearance
-  ];
-  gtk = {
-    theme = {
-      package = gtkThemeFromScheme {
-        scheme = colors;
-      };
-      name = colors.slug;
-    };
-    cursorTheme = {
-      package = pkgs.paper-icon-theme;
-      name = "Paper";
-    };
-    iconTheme = {
-      package = pkgs.paper-icon-theme;
-      name = "Paper";
-    };
-  };
-  programs.bash.bashrcExtra = ''
-    ${shellThemeFromScheme {
-      scheme = colors;
-    }}
-  '';
-  programs.terminator.config = {
-    profiles.default = {
-      background_color = "#${base00}";
-      foreground_color = "#${base05}";
-      cursor_color = "#${base06}";
-      pallete = concatStringsSep ":" (map (i: "#${i}") [
-        base00
-        base08
-        base0B
-        base0A
-        base0D
-        base0E
-        base0C
-        base06
-        "65737e"
-        base08
-        base0B
-        base0A
-        base0D
-        base0E
-        base0C
-        base07
-      ]);
-      font = "Monospace 10";
-      use_system_font = false;
-    };
-    global_config = {
-      title_transmit_fg_color = "#${base05}";
-      title_transmit_bg_color = "#${base02}";
-      title_inactive_fg_color = "#${base01}";
-      title_inactive_bg_color = "#${base00}";
-    };
-  };
+in {
   home.file."Downloads/theme.tdesktop-theme" = {
     source = pkgs.stdenv.mkDerivation {
       # borrowed from https://gitlab.com/theova/base16-telegram-desktop/-/blob/master/templates/default.mustache
---
+++ homes/main/theme/terminator.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, ... }:
+let
+  inherit (builtins) concatStringsSep;
+  inherit (pkgs.custom) colors;
+  inherit (colors.colors) base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F;
+in {
+  programs.terminator.config = {
+    profiles.default = {
+      background_color = "#${base00}";
+      foreground_color = "#${base05}";
+      cursor_color = "#${base06}";
+      pallete = concatStringsSep ":" (map (i: "#${i}") [
+        base00
+        base08
+        base0B
+        base0A
+        base0D
+        base0E
+        base0C
+        base06
+        "65737e"
+        base08
+        base0B
+        base0A
+        base0D
+        base0E
+        base0C
+        base07
+      ]);
+      font = "Monospace 10";
+      use_system_font = false;
+    };
+    global_config = {
+      title_transmit_fg_color = "#${base05}";
+      title_transmit_bg_color = "#${base02}";
+      title_inactive_fg_color = "#${base01}";
+      title_inactive_bg_color = "#${base00}";
+    };
+  };
+}
