6ceac399ea8aa82170af022f177c5dd5c282c72e
lucasew <lucas59356@gmail.com>
2022-09-16 10:41:48 -0300

    kubernetes: fix ip stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/whiterun/kubernetz.nix
+++ nodes/whiterun/kubernetz.nix
@@ -1,9 +1,18 @@
 {config, pkgs, ...}:
-{
+let
+  masterIp = "192.168.69.1";
+  masterAPIServerPort = 6443;
+in {
   services.kubernetes = {
     roles = [ "master" "node" ];
-    masterAddress = "localhost";
+    masterAddress = masterIp;
+    apiserverAddress = "http://${masterIp}:${toString masterAPIServerPort}";
     kubelet.extraOpts = "--fail-swap-on=false";
+    apiserver = {
+      securePort = masterAPIServerPort;
+      advertiseAddress = masterIp;
+    };
+    addons.dns.enable = true;
   };
   environment.etc."cni/net.d".enable = false;
   environment.etc."cni/net.d/11-flannel.conf".source = "${config.environment.etc."cni/net.d".source}/11-flannel.conf";
