7d349c4864ebad86639a8bad7a95817159060cef
lucasew <lucas59356@gmail.com>
2023-01-03 08:32:43 -0300

    whiterun: expose transmission through nginx
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/whiterun/transmission.nix
+++ nodes/whiterun/transmission.nix
@@ -1,9 +1,14 @@
-{ ... }: {
+{ config, ... }: {
   services.transmission = {
     enable = true;
     openFirewall = true;
     settings = {
       # incomplete-dir = "/tmp/transmission/incomplete";
+    };
+  };
+  services.nginx.virtualHosts."transmission.${config.networking.hostName}.${config.networking.domain}" = {
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.transmission.settings.rpc-port}";
     };
   };
 }
