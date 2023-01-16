2ddabf747a24fde5b5e8f1cc5b8e0d9e11a20742
lucasew <lucas59356@gmail.com>
2023-01-15 22:03:11 -0300

    expose ssh-alias
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/bootstrap/sops.nix
+++ nodes/bootstrap/sops.nix
@@ -1,6 +1,13 @@
-{ self, ... }:
+{ config, self, ... }:
 {
   imports = [
     self.inputs.sops-nix.nixosModules.sops
   ];
+
+  sops.secrets.ssh-alias = {
+    sopsFile = ../../secrets/ssh-alias;
+    owner = config.users.users.lucasew.name;
+    group = config.users.users.lucasew.group;
+    format = "binary";
+  };
 }
