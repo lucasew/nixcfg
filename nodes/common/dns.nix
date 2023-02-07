f3a7892dd1dc8b6e8455793151ada04232ddc196
lucasew <lucas59356@gmail.com>
2023-02-07 19:00:33 -0300

    dnsmasq: fix crash?
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nodes/common/dns.nix
+++ nodes/common/dns.nix
@@ -2,7 +2,7 @@
   services.dnsmasq = {
     enable = true;
     settings = {
-      servers = [ "8.8.8.8" "8.8.4.4" ];
+      server = [ "8.8.8.8" "8.8.4.4" ];
       domain-needed = true;
       bogus-priv = true;
       hostsdir = "/etc/extraHosts";
