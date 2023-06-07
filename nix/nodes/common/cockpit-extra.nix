57babff5ab7a79b73e1ca465dce2b22ce8d16546
lucasew <lucas59356@gmail.com>
2023-06-07 00:37:12 -0300

    cockpit seems broken on nonstandard port in nginx
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/cockpit-extra.nix
+++ nix/nodes/common/cockpit-extra.nix
@@ -1,17 +1,17 @@
 { self, config, lib, pkgs, ... }:
 
 lib.mkIf config.services.cockpit.enable {
-  networking.ports.cockpit.enable = true;
+  # networking.ports.cockpit.enable = true;
 
   services.nginx.virtualHosts."cockpit.${config.networking.hostName}.${config.networking.domain}" = {
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.networking.ports.cockpit.port}";
+      proxyPass = "http://127.0.0.1:${toString config.services.cockpit.port}";
       proxyWebsockets = true;
     };
   };
 
   services.cockpit = {
-    inherit (config.networking.ports.cockpit) port;
+    # inherit (config.networking.ports.cockpit) port;
     settings = {
       WebService = {
         Origins = "http://cockpit.${config.networking.hostName}.${config.networking.domain}";
