bf69771e2e69d30934d837f591af1df908615eb4
lucasew <lucas59356@gmail.com>
2023-06-06 23:31:56 -0300

    applying the port module in some stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/bootstrap/port-alloc.nix
+++ nix/nodes/bootstrap/port-alloc.nix
@@ -17,29 +17,25 @@ in
 
     description = "Build time port allocations for services that are only used internally";
 
+    apply = ports: lib.pipe ports [
+      (attrNames) # gets only the names of the ports
+      (foldl' (x: y: x // {
+        "${y}" = (ports.${y}) // {
+          port = x._port;
+        };
+        _port = x._port - 1;
+      })  {_port = 65534; }) # gets the count down of the ports
+      (x: removeAttrs x ["_port"]) # removes the utility _port entity
+    ];
+
     type = types.attrsOf (types.submodule ({ name, config, options, ... }: {
       options = {
         enable = mkEnableOption "Enable automatic port allocation for service ${name}";
         port = mkOption {
           description = "Allocated port for service ${name}";
           type = types.nullOr types.port;
-          default = null;
         };
       };
       }));
   };
-
-  imports = [
-    ({config, ...}: {
-      imports = lib.pipe config.networking.ports [
-        (attrNames) # gets only the names of the ports
-        (foldl' (x: y: x // { "${y}" = x._port; _port = x._port - 1; })  {_port = 65535; }) # gets the count down of the ports
-        (x: removeAttrs x ["_port"]) # removes the utility _port entity
-        (mapAttrs (k: v: {config, ...}: { # generates a module for each port but in an attrset
-            config.networking.ports.${k}.port = mkDefault v;
-        }))
-        (attrValues) # convert that module attrset to a list of modules
-      ];
-    })
-  ];
 }
--- nix/nodes/common/cockpit-extra.nix
+++ nix/nodes/common/cockpit-extra.nix
@@ -1,17 +1,21 @@
 { self, config, lib, pkgs, ... }:
 
 lib.mkIf config.services.cockpit.enable {
+  networking.ports.cockpit.enable = true;
 
   services.nginx.virtualHosts."cockpit.${config.networking.hostName}.${config.networking.domain}" = {
     locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.cockpit.port}";
+      proxyPass = "http://127.0.0.1:${toString config.networking.ports.cockpit.port}";
       proxyWebsockets = true;
     };
   };
 
-  services.cockpit.settings = {
-    WebService = {
-      Origins = "http://cockpit.${config.networking.hostName}.${config.networking.domain}";
+  services.cockpit = {
+    inherit (config.networking.ports.cockpit) port;
+    settings = {
+      WebService = {
+        Origins = "http://cockpit.${config.networking.hostName}.${config.networking.domain}";
+      };
     };
   };
 }
