72d40b4805dbb2d73606aef94e92859b3a1004e3
lucasew <lucas59356@gmail.com>
2023-07-30 11:15:14 -0300

    home/discord: use makeBinPath instead of glueing pkgs.stuff/bin
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/homes/main/theme/discord/default.nix
+++ nix/homes/main/theme/discord/default.nix
@@ -2,7 +2,7 @@
 {
   home.activation = {
     betterdiscord = lib.hm.dag.entryAfter ["writeBoundary"] ''
-PATH=${pkgs.custom.colorpipe}/bin:${pkgs.betterdiscordctl}/bin:$PATH
+PATH=${lib.makeBinPath (with pkgs; [ curl custom.colorpipe betterdiscordctl ])}:$PATH
 BETTERDISCORD_CSS_TEMPLATE=${./custom.css}
 ${builtins.readFile ./activation.sh}
     '';
