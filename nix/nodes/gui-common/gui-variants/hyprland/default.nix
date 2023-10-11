c74351003aea42c464b53c07004161b07ddcb56a
lucasew <lucas59356@gmail.com>
2023-10-11 00:44:29 -0300

    rofi: funcionando no hyprland
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/gui-variants/hyprland/default.nix
+++ nix/nodes/gui-common/gui-variants/hyprland/default.nix
@@ -1,7 +1,10 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   config = lib.mkIf config.programs.hyprland.enable {
     programs.regreet.enable = true;
+    environment.systemPackages = [
+      pkgs.custom.rofi
+    ];
   };
 }
