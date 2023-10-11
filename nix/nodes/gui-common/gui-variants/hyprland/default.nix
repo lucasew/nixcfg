4791100251ab0bd509ef84d480ee46b089785b27
lucasew <lucas59356@gmail.com>
2023-10-11 13:15:48 -0300

    hyprland: add portal
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/gui-variants/hyprland/default.nix
+++ nix/nodes/gui-common/gui-variants/hyprland/default.nix
@@ -17,6 +17,10 @@
       script = "blueberry-tray; while true; do sleep 3600; done";
     };
 
+    xdg.portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+    ];
+
     environment.systemPackages = with pkgs; [
       custom.rofi
       swaylock
