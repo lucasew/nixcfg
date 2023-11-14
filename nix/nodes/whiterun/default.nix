36f0da5c573e8f1df3ab14a576f7aa39582b3f33
lucasew <lucas59356@gmail.com>
2023-11-14 12:37:01 -0300

    whiterun: officialy unbreak my fstab
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/default.nix
+++ nix/nodes/whiterun/default.nix
@@ -40,7 +40,7 @@ in {
 
   fileSystems."/media/ssd240" = {
     device = "/dev/disk/by-label/ssd240";
-    fsType = "240";
+    fsType = "ext4";
   };
 
   fileSystems."/var/lib/transmission/Downloads" = {
