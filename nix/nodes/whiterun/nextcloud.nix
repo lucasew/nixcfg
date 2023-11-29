300e5795570957885b64a8a46820c1edcef888e2
lucasew <lucas59356@gmail.com>
2023-11-29 11:00:36 -0300

    nextcloud: working on some dependencies
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/nextcloud.nix
+++ nix/nodes/whiterun/nextcloud.nix
@@ -4,8 +4,12 @@ let
 in {
   config = mkIf config.services.nextcloud.enable {
     services.nextcloud.package = pkgs.nextcloud27;
-    users.users.nextcloud.extraGroups = [ "admin-password" ];
+    users.users.nextcloud = {
+      extraGroups = [ "admin-password" "render" ];
+      packages = with pkgs; [ ffmpeg nodejs coreutils exiftool ];
+    };
     services.nextcloud = {
+      configureRedis = true;
       hostName = "nextcloud.${config.networking.hostName}.${config.networking.domain}";
       config = {
         dbtype = "pgsql";
@@ -15,6 +19,19 @@ in {
         adminuser = "lucasew";
         adminpassFile = "/var/run/secrets/admin-password";
       };
+      extraOptions.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
     };
 
     systemd.services.nextcloud-setup = {
