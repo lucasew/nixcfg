8ca815c384a335c5729dcddbcdecde9609509ca9
lucasew <lucas59356@gmail.com>
2023-11-29 13:03:17 -0300

    nextcloud: hardcode dep paths
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/nextcloud.nix
+++ nix/nodes/whiterun/nextcloud.nix
@@ -6,7 +6,6 @@ in {
     services.nextcloud.package = pkgs.nextcloud27;
     users.users.nextcloud = {
       extraGroups = [ "admin-password" "render" ];
-      packages = with pkgs; [ ffmpeg nodejs coreutils exiftool ];
     };
     services.nextcloud = {
       configureRedis = true;
@@ -19,24 +18,41 @@ in {
         adminuser = "lucasew";
         adminpassFile = "/var/run/secrets/admin-password";
       };
-      extraOptions.enabledPreviewProviders = [
-        "OC\\Preview\\BMP"
-        "OC\\Preview\\GIF"
-        "OC\\Preview\\JPEG"
-        "OC\\Preview\\Krita"
-        "OC\\Preview\\MarkDown"
-        "OC\\Preview\\MP3"
-        "OC\\Preview\\OpenDocument"
-        "OC\\Preview\\PNG"
-        "OC\\Preview\\TXT"
-        "OC\\Preview\\XBitmap"
-        "OC\\Preview\\HEIC"
-      ];
+      extraOptions = {
+        preview_ffmpeg_path = lib.getExe pkgs.ffmpeg;
+        "memories.exiftool" = lib.getExe pkgs.exiftool;
+        "memories.ffmpeg_path" = lib.getExe' pkgs.ffmpeg "ffmpeg";
+        "memories.ffprobe_path" = lib.getExe' pkgs.ffmpeg "ffprobe";
+        "memories.vod.ffmpeg" = lib.getExe' pkgs.ffmpeg "ffmpeg";
+        "memories.vod.ffprobe" = lib.getExe' pkgs.ffmpeg "ffprobe";
+        recognize = {
+          nice_binary = lib.getExe' pkgs.coreutils "nice";
+        };
+        enabledPreviewProviders = [
+          "OC\\Preview\\BMP"
+          "OC\\Preview\\GIF"
+          "OC\\Preview\\JPEG"
+          "OC\\Preview\\Krita"
+          "OC\\Preview\\MarkDown"
+          "OC\\Preview\\MP3"
+          "OC\\Preview\\OpenDocument"
+          "OC\\Preview\\PNG"
+          "OC\\Preview\\TXT"
+          "OC\\Preview\\XBitmap"
+          "OC\\Preview\\HEIC"
+        ];
+      };
     };
 
     systemd.services.nextcloud-setup = {
       requires = ["postgresql.service"];
       after = ["postgresql.service"];
+
+      script = ''
+        # extra steps
+        ln -sf ${lib.getExe pkgs.nodejs} ${config.services.nextcloud.datadir}/store-apps/recognize/bin/node
+        ln -sf ${lib.getExe pkgs.exiftool} ${config.services.nextcloud.datadir}/store-apps/memories/bin-ext/exiftool/exiftool
+      '';
     };
 
     services.postgresqlBackup.databases = [ "nextcloud" ];
