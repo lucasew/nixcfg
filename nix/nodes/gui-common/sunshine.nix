5b91f6f3c3ca88ce40f304810e8f0c9a0a6879bb
lucasew <lucas59356@gmail.com>
2023-12-25 10:28:26 -0300

    sunshine: not using xrandr in the unit
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/sunshine.nix
+++ nix/nodes/gui-common/sunshine.nix
@@ -15,10 +15,7 @@ in
     environment.systemPackages = [ cfg.package ];
 
     systemd.user.services.sunshine = {
-      script = ''
-        xrandr --output $(xrandr  | grep [^s]connected | sed 's; ;\n;g'| head -n 1) --mode 1368x768
-        ${lib.getExe cfg.package}
-      '';
+      script = lib.getExe cfg.package;
       serviceConfig = {
         Restart = "on-failure";
       };
