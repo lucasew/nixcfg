b629ea94e55d2111ff24ff350ee89a8fae13d548
lucasew <lucas59356@gmail.com>
2024-05-24 17:04:07 -0300

    php: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/_shortcuts/php/routes.php
+++ bin/_shortcuts/php/routes.php
@@ -53,6 +53,88 @@ function mime_from_buffer($buffer) {
 }
 
 
+$INPUT_DATA = array_merge_recursive($_GET, $_POST);
+$ROUTE = parse_url($_SERVER["REQUEST_URI"])["path"];
+$IS_ROUTED = false;
+
+function mark_routed() {
+    global $IS_ROUTED;
+    $orig_VALUE = $IS_ROUTED;
+    $IS_ROUTED = true;
+    // return false;
+    return $orig_VALUE;
+}
+
+
+/**
+ * bring required variables to scope then require the new file
+ */
+function execphp(string $script) {
+    global $INPUT_DATA, $ROUTE;
+    require $script;
+}
+
+/**
+ * create a route that matches anything starting with $base_route
+ */
+function use_route(string $base_route, string $handler_script) {
+    global $ROUTE, $IS_ROUTED;
+    if (str_starts_with($ROUTE, $base_route)) {
+        if ($IS_ROUTED) return;
+        $ROUTE = substr($ROUTE, strlen($base_route));
+        execphp($handler_script);
+    }
+}
+
+/**
+ * create a route that matches exactly $selected_route
+ */
+function exact_route(string $selected_route, string $handler_script) {
+    global $ROUTE;
+    if (strcmp($ROUTE, $selected_route) == 0) {
+        if (mark_routed()) return;
+        execphp($handler_script);
+    }
+}
+
+/**
+ * create a route with route params, like /users/:user/info 
+ * and pass the route param with input_data
+ */
+function exact_with_route_param(string $selected_route, string $handler_script) {
+    global $INPUT_DATA, $ROUTE;
+    $preprocess = function (string $raw_route) {
+        $splitted = preg_split("/\//", $raw_route);
+        $splitted = array_filter($splitted, function($v, $k) {
+            return !is_empty_string($v);
+        }, ARRAY_FILTER_USE_BOTH);
+        return array_values($splitted);
+    };
+    $params_parts = $preprocess($selected_route);
+    $route_parts = $preprocess($ROUTE);
+    // log_httpd(json_encode($params_parts));
+    // log_httpd(json_encode($route_parts));
+
+    $extra_params = [];
+    if (count($params_parts) == count($route_parts)) {
+        for ($i = 0; $i < count($params_parts); $i++) {
+            if (str_starts_with($params_parts[$i], ":")) {
+                $extra_params[substr($params_parts[$i], 1)] = $route_parts[$i];
+            } else {
+                if (strcmp($params_parts[$i], $route_parts[$i]) != 0) {
+                    return;
+                }
+            }
+        }
+    } else {
+        return;
+    }
+    if (mark_routed()) return;
+    $INPUT_DATA = array_merge_recursive($INPUT_DATA, $extra_params);
+    execphp($handler_script);
+}
+
+
 $_HEADERS = array();
 function header(string $header) {
     global $_HEADERS;
@@ -65,20 +147,14 @@ set_header("Connection", "close");
 ob_start();
 
 ?>
-
-<h1>Hello, world</h1>
 <?php
-content_html();
+
+exact_route("/phpinfo", "phpinfo.php");
+exact_route("/image", "image.php");
+
+use_route("/", "index.php");
+
 ?>
-
-<pre>
-
-<?php
-phpinfo();
-?>
-
-</pre>
-
 <?php
 $data = ob_get_contents();
 ob_end_clean();
