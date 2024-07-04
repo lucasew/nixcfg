efff53cdcdf1bc9664d6e1bb188a768572f32a44
lucasew <lucas59356@gmail.com>
2024-07-04 10:34:50 -0300

    nix: configure autogc
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/bootstrap/nix.nix
+++ nix/nodes/bootstrap/nix.nix
@@ -1,10 +1,18 @@
-{ global, pkgs, ... }:
+{ global, pkgs, lib, ... }:
 let
   inherit (global) username;
 in
 {
   nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 3d --max-freed ${toString (10 * 1024*1024*1024)}";
+    };
+
     settings = {
+      min-free = lib.mkDefault (   5 * 1024 * 1024 * 1024);
+      max-free = lib.mkDefault (100 * 1024 * 1024 * 1024);
       trusted-users = [
         username
         "@wheel"
