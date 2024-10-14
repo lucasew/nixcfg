d4aee5fe9663e0693e0ee493a3896b4afa8b0c1c
lucasew <lucas59356@gmail.com>
2024-10-13 22:18:06 -0300

    ollama: ts-proxy
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/services/ollama.nix
+++ nix/nodes/common/services/ollama.nix
@@ -12,14 +12,27 @@ in
 {
 
   config = lib.mkIf cfg.enable {
+    services.ts-proxy.hosts = {
+      ollama = {
+        addr = "http://127.0.0.1:${toString config.networking.ports.ollama-web.port}";
+      };
+    };
+
     services.ollama = {
       acceleration = "cuda";
       listenAddress = "127.0.0.1:${toString config.networking.ports.ollama.port}";
     };
 
     networking.ports.ollama.enable = true;
+    networking.ports.ollama-web.enable = true;
 
     services.nginx.virtualHosts."ollama.${config.networking.hostName}.${config.networking.domain}" = {
+      listen = [
+        {
+          port = config.networking.ports.ollama-web.port;
+          addr = "127.0.0.1";
+        }
+      ];
       root = "${pkgs.ollama-webui}/share/ollama-webui/www";
       locations = {
         "/" = {
