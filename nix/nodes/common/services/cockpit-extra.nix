334b89df094d8ffbabce2b370fab01b7a46fae8a
lucasew <lucas59356@gmail.com>
2024-10-15 20:24:32 -0300

    cockpit: use default port, no vhost
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/services/cockpit-extra.nix
+++ nix/nodes/common/services/cockpit-extra.nix
@@ -1,27 +1,17 @@
 {
-  self,
   config,
   lib,
-  pkgs,
   ...
 }:
 
 lib.mkIf config.services.cockpit.enable {
   # networking.ports.cockpit.enable = true;
 
-  services.nginx.virtualHosts."cockpit.${config.networking.hostName}.${config.networking.domain}" = {
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.cockpit.port}";
-      proxyWebsockets = true;
-    };
-  };
-
   services.cockpit = {
     # inherit (config.networking.ports.cockpit) port;
     settings = {
       WebService = {
         Origins = builtins.concatStringsSep " " [
-          "http://cockpit.${config.networking.hostName}.${config.networking.domain}"
           "http://${config.networking.hostName}:${toString config.services.cockpit.port}"
           "https://${config.networking.hostName}:${toString config.services.cockpit.port}"
           "http://${config.networking.hostName}:${toString config.services.cockpit.port}"
