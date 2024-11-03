62d399e586cce620449150e226677873bb875a12
lucasew <lucas59356@gmail.com>
2024-11-02 22:01:45 -0300

    minecraft: backup script
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/services/minecraft.nix
+++ nix/nodes/common/services/minecraft.nix
@@ -8,6 +8,28 @@ in
   config = lib.mkIf config.services.minecraft-server.enable {
     networking.ports.minecraft.enable = true;
 
+    systemd.services.minecraft-server-backup = {
+      description = "Minecraft server backup";
+      path = [ pkgs.zip ];
+      script = ''
+        function rcon {
+          if [[ -p /run/minecraft-server.stdin ]]; then
+            echo "$@" | tee /run/minecraft-server.stdin
+          fi
+        }
+        rcon /say Backup iniciado
+        rcon /save-off
+        rcon /save-all
+        sleep 15
+
+        cd "${config.services.minecraft-server.dataDir}"
+        zip -9 -r /var/backup/minecraft.zip world || rcon /say Backup falhou, olha os logs carai
+
+        rcon /save-on
+        rcon /say Backup feito
+      '';
+    };
+
     services.minecraft-server = {
       package = pkgs.unstable.minecraftServers.vanilla-1-15.override {
         jre_headless = pkgs.unstable.openjdk8;
