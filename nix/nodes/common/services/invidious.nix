067383fbdbdf58c1d37be1b8f086e9751006c14b
lucasew <lucas59356@gmail.com>
2024-11-06 23:07:52 -0300

    invidious: generating those token stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/services/invidious.nix
+++ nix/nodes/common/services/invidious.nix
@@ -3,8 +3,17 @@
 lib.mkIf config.services.invidious.enable {
   networking.ports.invidious.enable = true;
   # networking.ports.invidious.port = lib.mkDefault 49149;
+  systemd.services.invidious.serviceConfig.DynamicUser = lib.mkForce false;
+
+  sops.secrets."invidious" = {
+    sopsFile = ../../../secrets/invidious.txt;
+    owner = "invidious";
+    group = "invidious";
+    format = "binary";
+  };
   services.invidious = {
     package = pkgs.unstable.invidious;
+    extraSettingsFile = "/var/run/secrets/invidious";
     inherit (config.networking.ports.invidious) port;
     settings = {
       db = {
@@ -12,6 +21,14 @@ lib.mkIf config.services.invidious.enable {
         dbname = "invidious";
       };
     };
+  };
+
+  users = {
+    users.invidious = {
+      isSystemUser = true;
+      group = "invidious";
+    };
+    groups.invidious = {};
   };
 
   services.ts-proxy.hosts = {
