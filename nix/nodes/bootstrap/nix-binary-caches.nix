92cbfe1a1cb05ee68bd335d8bc19765cfa45806b
lucasew <lucas59356@gmail.com>
2024-11-18 16:57:09 -0300

    nixos: add garnix binary cache
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/bootstrap/nix-binary-caches.nix
+++ nix/nodes/bootstrap/nix-binary-caches.nix
@@ -5,12 +5,14 @@
       "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
       "https://cuda-maintainers.cachix.org"
+      "https://cache.garnix.io"
       # "http://nix-cache.whiterun.lucao.net"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 }
