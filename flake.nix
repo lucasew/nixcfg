0d7b6464073306d0570327588163d9201d3d4157
lucasew <lucas59356@gmail.com>
2024-12-09 21:28:26 -0300

    flake: add binary caches here too
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,21 @@
 {
   description = "nixcfg";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://devenv.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://lucasew-personal.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "lucasew-personal.cachix.org-1:sGVvGjt2TiYjRacwboM4dbxjX036rsZwjgDG+NKgGe8="
+    ];
+  };
+
   inputs = {
     blender-bin.url = "blender-bin";
     blender-bin.inputs.nixpkgs.follows = "nixpkgs";
