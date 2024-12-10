f0906e49b7857207149502ead567f0edab9651bb
lucasew <lucas59356@gmail.com>
2024-12-09 21:33:39 -0300

    nixos: unify substituter settings
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/bootstrap/nix-binary-caches.nix
+++ nix/nodes/bootstrap/nix-binary-caches.nix
@@ -1,19 +1,10 @@
 { lib, ... }:
+let
+  flakeRaw = import ../../../flake.nix;
+in
 {
   nix.settings = {
-    substituters = lib.mkAfter [
-      "https://nix-community.cachix.org"
-      "https://devenv.cachix.org"
-      "https://cuda-maintainers.cachix.org"
-      "https://lucasew-personal.cachix.org"
-      # "https://cache.garnix.io"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "lucasew-personal.cachix.org-1:sGVvGjt2TiYjRacwboM4dbxjX036rsZwjgDG+NKgGe8="
-      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
+    substituters = lib.mkAfter flakeRaw.nixConfig.extra-substituters;
+    trusted-public-keys = lib.mkAfter flakeRaw.nixConfig.extra-trusted-public-keys;
   };
 }
