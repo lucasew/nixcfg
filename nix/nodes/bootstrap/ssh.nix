9363a49b0367a64c4579aec5d74e5964288b09ac
lucasew <lucas59356@gmail.com>
2025-01-12 10:45:32 -0300

    add ssh hosts
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/bootstrap/ssh.nix
+++ nix/nodes/bootstrap/ssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   services.openssh = {
     enable = true;
@@ -6,5 +6,16 @@
       PasswordAuthentication = true;
     };
   };
+
+  programs.ssh.extraConfig = ''
+    Include ${config.sops.secrets.ssh-hosts.path}
+  '';
   programs.mosh.enable = true;
+
+  sops.secrets.ssh-hosts = {
+    sopsFile = ../../../secrets/ssh-hosts;
+    owner = config.users.users.lucasew.name;
+    group = config.users.users.lucasew.group;
+    format = "binary";
+  };
 }
