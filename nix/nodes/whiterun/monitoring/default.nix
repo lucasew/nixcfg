5bdfbe9fe8953acc0ac38d21b9468754de20f7b1
lucasew <lucas59356@gmail.com>
2025-05-18 13:43:27 -0300

    rm grafana
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/monitoring/default.nix
+++ nix/nodes/whiterun/monitoring/default.nix
@@ -12,22 +12,7 @@
     ./nvidia.nix
   ];
 
-  networking.ports.grafana-web.enable = true;
-  # networking.ports.grafana-web.port = lib.mkDefault 49150;
-  services.grafana = {
-    enable = true;
-    settings.server = {
-      domain = "grafana.${config.services.ts-proxy.network-domain}";
-      http_port = config.networking.ports.grafana-web.port;
-      http_addr = "127.0.0.1";
-    };
-  };
-
   services.ts-proxy.hosts = {
-    grafana = {
-      address = "127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
-      enableTLS = true;
-    };
     prometheus = {
       address = "127.0.0.1:${toString config.services.prometheus.port}";
       enableTLS = true;
