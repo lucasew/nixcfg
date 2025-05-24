7abdbe654b2fc603f12f1bf5bde9a5777296b397
lucasew <lucas59356@gmail.com>
2025-05-24 12:10:19 -0300

    FINALLY
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/gui-variants/sway/default.nix
+++ nix/nodes/gui-common/gui-variants/sway/default.nix
@@ -228,6 +228,11 @@ in
         default_border pixel 2
         hide_edge_borders smart
         focus_on_window_activation urgent
+
+        # announce a running sway session to systemd
+        # https://discourse.nixos.org/t/xdg-desktop-portal-not-working-on-wayland-while-kde-is-installed/20919/3
+        exec systemctl --user import-environment XDG_SESSION_TYPE XDG_CURRENT_DESKTOP
+        exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
       '';
   };
 }
