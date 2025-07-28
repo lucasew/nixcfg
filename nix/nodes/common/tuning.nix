06f81b3df4c4ef35ab12d721dd155b159205cda6
lucasew <lucas59356@gmail.com>
2025-07-28 15:27:50 -0300

    nixos/tuning: rm ananicy
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/tuning.nix
+++ nix/nodes/common/tuning.nix
@@ -1,15 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
   boot = {
     kernel.sysctl = {
       "vm.swappiness" = 10;
     };
     tmp.cleanOnBoot = true;
-  };
-  services = {
-    ananicy = {
-      enable = true;
-      package = pkgs.ananicy-cpp;
-    };
   };
 }
