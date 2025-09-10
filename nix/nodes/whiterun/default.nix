393cbb04c41442e61fde63971782e1f0306983a3
lucasew <lucas59356@gmail.com>
2025-09-10 15:57:12 -0300

    rm slurm
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/default.nix
+++ nix/nodes/whiterun/default.nix
@@ -34,19 +34,6 @@ in
 
   stylix.enable = true;
 
-  services.slurm = {
-    server.enable = true;
-    client.enable = true;
-    clusterName = "lucaoenterprise";
-    controlMachine = "whiterun";
-    nodeName = [
-      "whiterun CPUs=12 State=UNKNOWN"
-    ];
-    partitionName = [
-      "standard Nodes=whiterun Default=YES MaxTime=INFINITE State=UP"
-    ];
-  };
-
   services.minecraft-server.enable = true;
 
   services.phpelo.enable = true;
