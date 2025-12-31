692285a0ed4636803a6dd2adaf3e69af8a2b0eec
lucasew <lucas59356@gmail.com>
2025-12-31 15:14:34 -0300

    nvidia default
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/whiterun/nvidia.nix
+++ nix/nodes/whiterun/nvidia.nix
@@ -22,16 +22,6 @@
   hardware.nvidia-container-toolkit = {
     enable =
       config.virtualisation.docker.enable || config.virtualisation.podman.enable;
-    package = pkgs.unstable.nvidia-container-toolkit;
-  };
-
-
-  virtualisation.docker = {
-    daemon.settings = {
-      runtimes.nvidia = {
-        path = "${pkgs.nvidia-container-toolkit.tools}/bin/nvidia-container-runtime";
-      };
-    };   
   };
 
   environment.systemPackages = [ pkgs.nvtopPackages.full ];
