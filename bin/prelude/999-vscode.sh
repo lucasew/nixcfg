a2a70551523940f391f989d23e5a122e6ce4f423
lucasew <lucas59356@gmail.com>
2026-01-11 09:56:12 -0300

    use code shim
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/prelude/999-vscode.sh
+++
@@ -1,7 +0,0 @@
-function codew {
-  flatpak --socket=wayland run com.visualstudio.code --enable-features=UseOzonePlatform --ozone-platform-hint=auto "$@"
-}
-
-function code {
-  "$(which code)"  --enable-features=UseOzonePlatform --ozone-platform-hint=auto "$@"
-}
