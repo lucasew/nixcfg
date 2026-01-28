90c9d047c82af5db8d58a346873200b7d4dd0179
lucasew <lucas59356@gmail.com>
2026-01-28 13:29:35 -0300

    workspaced: dont listen socket
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/workspaced.nix
+++ nix/nodes/gui-common/workspaced.nix
@@ -4,12 +4,6 @@
   config = {
     environment.systemPackages = [ pkgs.workspaced ];
 
-    systemd.user.sockets.workspaced = {
-      description = "Workspaced Socket";
-      wantedBy = [ "sockets.target" ];
-      listenStreams = [ "%t/workspaced.sock" ];
-    };
-
     systemd.user.services.workspaced = {
       description = "Workspaced Daemon";
       wantedBy = [ "graphical-session.target" ];
