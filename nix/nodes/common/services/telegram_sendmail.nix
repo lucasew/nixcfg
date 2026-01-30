a4c6cf3b87a6ca965570cdeee5c8c1d1c2b9330e
lucasew <lucas59356@gmail.com>
2026-01-30 19:14:02 -0300

    telegram sendmail
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/common/services/telegram_sendmail.nix
+++ nix/nodes/common/services/telegram_sendmail.nix
@@ -13,10 +13,6 @@ in
 
   config = mkIf config.services.telegram-sendmail.enable {
     services.telegram-sendmail.credentialFile = "/var/run/secrets/telegram-sendmail";
-    services.telegram-sendmail.extraArgs = [
-      "-t"
-      "120"
-    ];
 
     sops.secrets.telegram-sendmail = {
       sopsFile = ../../../../secrets/telegram_sendmail.env;
