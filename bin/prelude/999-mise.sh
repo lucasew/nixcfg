fdbd9c8f74b9546c4bb25961a238b09324480fd4
lucasew <lucas59356@gmail.com>
2026-01-29 23:51:22 -0300

    unset -f mise
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/prelude/999-mise.sh
+++ bin/prelude/999-mise.sh
@@ -3,5 +3,7 @@ export MISE_ALL_COMPILE=false
 
 if [ -f "$HOME/.local/bin/mise" ]; then
 	eval "$("$HOME"/.local/bin/mise activate bash)"
-
+	if [ -n "${TERMUX_VERSION:-}" ]; then
+		unset -f mise
+	fi
 fi
