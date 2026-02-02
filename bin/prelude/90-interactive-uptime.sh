be3485bb56873898f1227cdfa5d77da015d567a1
lucasew <lucas59356@gmail.com>
2026-02-02 13:28:34 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/prelude/90-interactive-uptime.sh
+++ bin/prelude/90-interactive-uptime.sh
@@ -1,8 +1,4 @@
 # shellcheck shell=bash
 if [ $TERM != dumb ]; then
-
-	if [ ! -v SD_CMD ]; then
-		printf '\033[1mUptime\033[0m: %s\n' "$(uptime)" || true
-	fi
-
+	printf '\033[1mUptime\033[0m: %s\n' "$(uptime)" || true
 fi
