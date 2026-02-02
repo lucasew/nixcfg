733e54d73e7088fd9f4d1b2271249a18072564f4
lucasew <lucas59356@gmail.com>
2026-02-02 11:21:20 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/prelude/15-workspaced-init.source.sh
+++ bin/prelude/15-workspaced-init.source.sh
@@ -8,12 +8,9 @@ if command -v workspaced >/dev/null 2>&1; then
 	# Generate completion
 	workspaced completion bash
 
-	# Apply colors directly to terminal if command exists (async to not block)
-	cat <<'EOF'
-
 	# Apply colors in interactive shell
-	if [[ $- == *i* ]] && workspaced colors --help &>/dev/null; then
-		(workspaced colors &) &>/dev/null
-	fi
-EOF
+	echo ""
+	echo "if [[ \$- == *i* ]] && workspaced colors --help &>/dev/null; then"
+	echo "	workspaced colors"
+	echo "fi"
 fi
