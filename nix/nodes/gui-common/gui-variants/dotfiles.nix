819a852e277d0c79a45dbc9c7a6002f1051988b3
lucasew <lucas59356@gmail.com>
2026-02-08 13:29:56 -0300

    fix waybar theme
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nodes/gui-common/gui-variants/dotfiles.nix
+++ nix/nodes/gui-common/gui-variants/dotfiles.nix
@@ -1,22 +1,6 @@
 {pkgs, ...}:
 
 {
-    systemd.user.services.dotfile-waybar = {
-      path = with pkgs; [
-        script-directory-wrapper
-        custom.colorpipe
-        bash
-      ];
-      script = ''
-        mkdir ~/.config/waybar -p
-        cat $(sdw d root)/nix/nodes/gui-common/gui-variants/hyprland/waybar/style.css | colorpipe > ~/.config/waybar/style.css
-      '';
-      restartTriggers = [
-        ./hyprland/waybar/style.css
-      ];
-      wantedBy = [ "default.target" ];
-    };
-
     systemd.user.services.dotfile-hyprland = {
       path = with pkgs; [
         script-directory-wrapper
