357c1474aefa8c883341ab9a871a497d6a9644fd
lucasew <lucas59356@gmail.com>
2026-02-10 16:43:22 -0300

    fix prelude join bug
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- config/.bashrc.d.tmpl/30-integrations-ssh-agent.sh
+++ config/.bashrc.d.tmpl/30-integrations-ssh-agent.sh
@@ -1,8 +1,7 @@
 # shellcheck shell=bash
-if [[ -S "${SSH_AUTH_SOCK:-}" ]]; then
-	return
-fi
-
-if [[ -S "$XDG_RUNTIME_DIR/gcr/ssh" ]]; then
-	export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+# Only set SSH_AUTH_SOCK if not already set
+if [[ ! -S "${SSH_AUTH_SOCK:-}" ]]; then
+	if [[ -S "$XDG_RUNTIME_DIR/gcr/ssh" ]]; then
+		export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+	fi
 fi
