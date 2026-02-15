70208c46b68af149ca0cd361c55d0fd913dabfcc
lucasew <lucas59356@gmail.com>
2026-02-15 10:45:27 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- config/.bashrc.d.tmpl/00-core-bin-path.sh
+++ config/.bashrc.d.tmpl/00-core-bin-path.sh
@@ -1,16 +1,2 @@
 # shellcheck shell=bash
-_local_bin="$HOME/.local/bin"
-
-# Remove existing ~/.local/bin from PATH to avoid duplicates
-_clean_path=":$PATH:"
-_clean_path="${_clean_path//:$_local_bin:/:}"
-_clean_path="${_clean_path#:}"
-_clean_path="${_clean_path%:}"
-
-# Prepend ~/.local/bin (global shims including 'x' helper)
-export PATH="$_local_bin:$_clean_path"
-
-unset _local_bin _clean_path
-
-# Note: Lazy shims in ~/.local/share/workspaced/shim/lazy/
-# are added to PATH via 'x' wrapper when needed
+export PATH="$PATH:$HOME/.local/bin"
