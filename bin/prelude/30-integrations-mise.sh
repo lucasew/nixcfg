936a20f38fa2883cfe8acb2809c5714966353a0f
lucasew <lucas59356@gmail.com>
2026-02-16 11:59:15 -0300

    mise setup
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- bin/prelude/30-integrations-mise.sh
+++ bin/prelude/30-integrations-mise.sh
@@ -1,6 +1,8 @@
 # shellcheck shell=bash
 # Mise configuration - activation code generated inline in Go
 
+eval <(mise activate || true)
+
 export MISE_ALL_COMPILE=false
 
 # Unset mise function (from mise activate) to use binary
